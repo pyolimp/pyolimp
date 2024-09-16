aa6f7f1a642257adb8ff4d29d4769bc6c4a74bd1
Dmitry Nikolaev <d.p.nikolaev@gmail.com>
2024-09-16 18:40:20 +0300

    feat: simulate: generalize distortion class

--- olimp/simulate/simulat.py
+++ olimp/simulate/simulat.py
@@ -1,4 +1,4 @@
-from typing import Literal, TYPE_CHECKING, NamedTuple
+from typing import Literal, TYPE_CHECKING, Any
 
 import torch
 from abc import ABC, abstractmethod
@@ -7,48 +7,22 @@ from abc import ABC, abstractmethod
 from torch import Tensor
 
 
-class CVDSimulationParams(NamedTuple):
-    sim_name: Literal["Vienot", "Huang", "Farup"]
-    cvd_type: Literal["PROTAN", "DEUTAN", "rg"]
-
-
-def simulate(image: Tensor, params: Tensor | CVDSimulationParams) -> Tensor:
-    if isinstance(params, Tensor):
-        sim = SimulateRetinalImage()
-    elif isinstance(params, CVDSimulationParams):
-        sim = SimulateDichromacy()
-    else:
-        raise ValueError(params)
-    return sim(image, params)
-
-
-class Simulate(ABC):
+class Distortion(ABC):
     @abstractmethod
-    def __call__(
-        self, image: Tensor, params: Tensor | CVDSimulationParams
-    ) -> Tensor:
+    def __init__(self, *args: Any) -> None:
         raise NotImplementedError
 
-    # @abstractmethod
-    # def fast_conv(
-    #     self, I: Tensor, K: Tensor, return_2d: bool = False
-    # ) -> Tensor:
-    #     """
-    #     Perform convolution using FFT on the input tensor I with the kernel tensor K.
-
-    #     Parameters:
-    #     I (Tensor): Input image tensor of shape (b, n, w, h), (c, w, h), or (w, h) with torch.float data type.
-    #     K (Tensor): PSF kernel tensor of shape (b, 1, w, h), (1, w, h), or (w, h) with torch.float data type.
-    #     return_2d (bool): Whether to return a 2D tensor if input is 2D. Default is False.
-
-    #     Returns:
-    #     Tensor: Convolved tensor with the same spatial dimensions as input tensor I.
-    #     """
-    #     pass
+    @abstractmethod
+    def __call__(self, I: Tensor) -> Tensor:
+        raise NotImplementedError
 
 
-class SimulateRetinalImage(Simulate):
-    def __call__(self, I: Tensor, K: Tensor) -> Tensor:
+class RefractionDistortion(Distortion):
+    def __init__(self, K: Tensor) -> None:
+        assert self.K.dtype == torch.float, "K must have data type torch.float"
+        self.K = K
+
+    def __call__(self, I: Tensor) -> Tensor:
         """
         Perform convolution using FFT on the input tensor I with the kernel tensor K.
 
@@ -62,37 +36,40 @@ class SimulateRetinalImage(Simulate):
         """
         # Check data type
         assert I.dtype == torch.float, "I must have data type torch.float"
-        assert K.dtype == torch.float, "K must have data type torch.float"
 
         # Check value range
         assert torch.all(
             (I >= 0) & (I <= 1)
         ), "Values in I must be in the range [0, 1]"
         assert torch.all(
-            (K >= 0) & (K <= 1)
+            (self.K >= 0) & (self.K <= 1)
         ), "Values in K must be in the range [0, 1]"
 
         original_ndim = I.ndim  # Store the original number of dimensions
 
         # Handle different input shapes
-        if I.ndim == 2 and K.ndim == 2:
+        if I.ndim == 2 and self.K.ndim == 2:
             I = I.unsqueeze(0).unsqueeze(0)  # Convert (w, h) to (1, 1, w, h)
-            K = K.unsqueeze(0).unsqueeze(0)  # Convert (w, h) to (1, 1, w, h)
-        elif I.ndim == 3 and K.ndim == 3:
+            self.K = self.K.unsqueeze(0).unsqueeze(
+                0
+            )  # Convert (w, h) to (1, 1, w, h)
+        elif I.ndim == 3 and self.K.ndim == 3:
             I = I.unsqueeze(0)  # Convert (c, w, h) to (1, c, w, h)
-            K = K.unsqueeze(0)  # Convert (1, w, h) to (1, 1, w, h)
+            self.K = self.K.unsqueeze(0)  # Convert (1, w, h) to (1, 1, w, h)
 
         # Check dimensions after potential unsqueezing
         assert I.ndim == 4, "I must be a 4-dimensional tensor (b, n, w, h)"
-        assert K.ndim == 4, "K must be a 4-dimensional tensor (b, 1, w, h)"
         assert (
-            I.shape[2] == K.shape[2] and I.shape[3] == K.shape[3]
+            self.K.ndim == 4
+        ), "K must be a 4-dimensional tensor (b, 1, w, h)"
+        assert (
+            I.shape[2] == self.K.shape[2] and I.shape[3] == self.K.shape[3]
         ), "The width and height of I and K must match"
 
         # Handling batch size
-        if I.shape[0] != K.shape[0]:
-            if K.shape[0] == 1:
-                K = K.expand(I.shape[0], -1, -1, -1)
+        if I.shape[0] != self.K.shape[0]:
+            if self.K.shape[0] == 1:
+                self.K = self.K.expand(I.shape[0], -1, -1, -1)
             else:
                 assert (
                     False
@@ -101,7 +78,7 @@ class SimulateRetinalImage(Simulate):
         b, n, w, h = I.shape
 
         # Expand K to match the channel dimension of I
-        K_expanded = K.expand(-1, n, -1, -1)
+        K_expanded = self.K.expand(-1, n, -1, -1)
 
         # Apply FFT to both the image tensor and the PSF tensor
         I_fft = torch.fft.fft2(I)
@@ -123,7 +100,25 @@ class SimulateRetinalImage(Simulate):
         return conv_result
 
 
-class SimulateDichromacy(Simulate):
+class ColorBlindnessDistortion(Distortion):
+    def __init__(
+        self,
+        sim_type: Literal["Vienot", "Huang", "Farup"],
+        blindness_type: Literal["PROTAN", "DEUTAN", "rg"],
+    ) -> None:
+        assert sim_type in [
+            "Vienot",
+            "Huang",
+            "Farup",
+        ], "no such distortion"
+        assert blindness_type in [
+            "PROTAN",
+            "DEUTAN",
+            "rg",
+        ], "no such distortion"
+        self.sim_type = sim_type
+        self.blindness_type = blindness_type
+
     @staticmethod
     def _local_change_range(image: Tensor, quantile: float = 0.98):
         max_channel = torch.max(image, axis=2)[0]
@@ -157,7 +152,7 @@ class SimulateDichromacy(Simulate):
 
     @staticmethod
     def _convert_from_sRGB(sRGB: Tensor) -> Tensor:
-        linRGB = SimulateDichromacy._linearRGB_from_sRGB(sRGB)
+        linRGB = ColorBlindnessDistortion._linearRGB_from_sRGB(sRGB)
         LMS_from_RGB = torch.tensor(
             [
                 [0.27293945, 0.66418685, 0.06287371],
@@ -179,21 +174,15 @@ class SimulateDichromacy(Simulate):
             ]
         )
         inv_linRGB = LMS @ RGB_from_LMS.T
-        sRGB = SimulateDichromacy._sRGB_from_linearRGB(inv_linRGB)
+        sRGB = ColorBlindnessDistortion._sRGB_from_linearRGB(inv_linRGB)
         return sRGB
 
     @staticmethod
-    def _simulate(image: Tensor, cvd_params: CVDSimulationParams) -> Tensor:
-        assert cvd_params.sim_name in [
-            "Vienot",
-            "Huang",
-            "Farup",
-        ], "no such simulation"
-        assert cvd_params.cvd_type in [
-            "PROTAN",
-            "DEUTAN",
-            "rg",
-        ], "no such simulation"
+    def _simulate(
+        image: Tensor,
+        sim_type: Literal["Vienot", "Huang", "Farup"],
+        blindness_type: Literal["PROTAN", "DEUTAN", "rg"],
+    ) -> Tensor:
         protan_Vienot = torch.tensor(
             [[0.0, 1.06481845, -0.06481845], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
         )
@@ -210,60 +199,41 @@ class SimulateDichromacy(Simulate):
             [[0.5, 0.5, 0.0], [0.5, 0.5, 0.0], [0.0, 0.0, 1.0]]
         )
 
-        if cvd_params.cvd_type == "rg" and cvd_params.sim_name == "Farup":
+        if blindness_type == "rg" and sim_type == "Farup":
             return image @ FarupRGmatrix.T
-        else:
-            lms = SimulateDichromacy._convert_from_sRGB(image)
-            if cvd_params.cvd_type == "PROTAN":
-                if cvd_params.sim_name == "Vienot":
+        elif blindness_type in ("PROTAN", "DEUTAN") and sim_type in (
+            "Vienot",
+            "Huang",
+        ):
+            lms = ColorBlindnessDistortion._convert_from_sRGB(image)
+            if blindness_type == "PROTAN":
+                if sim_type == "Vienot":
                     dichromat_LMS = lms @ protan_Vienot.T
-                elif cvd_params.sim_name == "Huang":
+                elif sim_type == "Huang":
                     dichromat_LMS = lms @ protan_Huang.T
-            elif cvd_params.cvd_type == "DEUTAN":
-                if cvd_params.sim_name == "Vienot":
+            elif blindness_type == "DEUTAN":
+                if sim_type == "Vienot":
                     dichromat_LMS = lms @ deutan_Vienot.T
-                elif cvd_params.sim_name == "Huang":
+                elif sim_type == "Huang":
                     dichromat_LMS = lms @ deutan_Huang.T
-            sRGB = SimulateDichromacy._convert_from_LMS(dichromat_LMS)
+            sRGB = ColorBlindnessDistortion._convert_from_LMS(dichromat_LMS)
             sRGB = torch.clip(sRGB, 0, 1)
             return sRGB
+        else:
+            raise NotImplementedError
 
-    def __call__(
-        self, batch: Tensor, cvd_params: CVDSimulationParams
-    ) -> Tensor:
-        if batch.ndim == 3:
-            batch = batch[None]
-        batch_sim = torch.zeros_like(batch, dtype=torch.float)
-        for idx, image in enumerate(batch):
+    def __call__(self, I: Tensor) -> Tensor:
+        assert I.ndim > 2
+        if I.ndim == 3:
+            I = I[None]
+        I_sim = torch.zeros_like(I, dtype=torch.float)
+        for idx, image in enumerate(I):
             image = image - image.min()
             image = image / image.max()
-            image_normalized, _ = SimulateDichromacy._local_change_range(
-                image, 0.98
+            image_normalized, _ = ColorBlindnessDistortion._local_change_range(
+                image.permute(1, 2, 0), 0.98
             )
-            batch_sim[idx] = SimulateDichromacy._simulate(
-                image_normalized, cvd_params
-            )
-            # breakpoint()
-        return batch_sim
-
-
-if __name__ == "__main__":
-    I_2d = torch.rand(64, 64, dtype=torch.float)
-    K_2d = torch.rand(64, 64, dtype=torch.float)
-    result_2d = simulate(I_2d, K_2d)
-    print(result_2d.shape)  # Expected shape (64, 64)
-
-    I_2d = torch.rand(64, 64, dtype=torch.float)
-    K_2d = torch.rand(64, 64, dtype=torch.float)
-    result_2d = simulate(I_2d, K_2d)
-    print(result_2d.shape)  # Expected shape (1, 1, 64, 64)
-
-    I_3d = torch.rand(3, 64, 64, dtype=torch.float)
-    K_3d = torch.rand(1, 64, 64, dtype=torch.float)
-    result_3d = simulate(I_3d, K_3d)
-    print(result_3d.shape)  # Expected shape (1, 3, 64, 64)
-
-    I_4d = torch.rand(2, 3, 64, 64, dtype=torch.float)
-    K_4d = torch.rand(2, 1, 64, 64, dtype=torch.float)
-    result_4d = simulate(I_4d, K_4d)
-    print(result_4d.shape)  # Expected shape (2, 3, 64, 64)
+            I_sim[idx] = ColorBlindnessDistortion._simulate(
+                image_normalized, self.sim_type, self.blindness_type
+            ).permute(2, 0, 1)
+        return I_sim
