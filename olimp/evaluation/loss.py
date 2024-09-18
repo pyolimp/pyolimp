459a25d5c8509bb3edf391b2aee74007c442bfdb
Dmitry Nikolaev <d.p.nikolaev@gmail.com>
2024-09-18 12:02:33 +0300

    feat: loss: get rid of external color libs

--- olimp/evaluation/loss.py
+++ olimp/evaluation/loss.py
@@ -3,8 +3,10 @@ from typing import Literal
 from piq import MultiScaleSSIMLoss
 import torch
 from torch import Tensor
-from skimage import color
-import colour
+
+from .cs.srgb import sRGB
+from .cs.cielab import CIELAB
+from .cs.prolab import ProLab
 
 
 def ms_ssim(pred: Tensor, target: Tensor) -> Tensor:
@@ -65,48 +67,15 @@ def projective_transformation(points: Tensor, proj_matrix: Tensor) -> Tensor:
     return projection
 
 
-class ProLab:
-    def __init__(
-        self, illuminant_XYZ: Tensor = torch.tensor([0.95047, 1.0, 1.08883])
-    ):
-        self.illuminant_XYZ = illuminant_XYZ
-        self.Q = torch.tensor(
-            [
-                [75.5644333, 486.62630402, 167.39926268, 0.0],
-                [617.72787198, -595.4607401, -22.26712291, 0.0],
-                [48.3448951, 194.93477285, -243.27966363, 0.0],
-                [0.7554, 3.8666, 1.6739, 1.0],
-            ]
-        )
-
-    def from_XYZ(self, xyz: Tensor) -> Tensor:
-        xyz_norm = xyz.reshape(-1, xyz.shape[-1])
-        xyz_norm = xyz_norm / self.illuminant_XYZ
-        ucs_crd = projective_transformation(xyz_norm, self.Q)
-        ucs_crd = ucs_crd.view(xyz.shape[:-1] + ucs_crd.shape[-1:])
-        return ucs_crd
-
-    def to_XYZ(self, ucs_crd: Tensor) -> Tensor:
-        inv_Q = torch.linalg.inv(self.Q)
-        ucs_crd_2d = ucs_crd.reshape(-1, ucs_crd.shape[-1])
-        xyz = projective_transformation(ucs_crd_2d, inv_Q)
-        xyz = xyz * self.illuminant_XYZ
-        xyz = xyz.reshape(ucs_crd.shape[:-1] + xyz.shape[-1:])
-        return xyz
-
-
-ref_prolab = ProLab()
-
-
 def srgb2prolab(srgb: Tensor) -> Tensor:
-    xyz: Tensor = color.rgb2xyz(srgb)
-    prolab = ref_prolab.from_XYZ(xyz)
+    xyz: Tensor = sRGB()._to_XYZ(srgb)
+    prolab = ProLab(torch.tensor([0.95047, 1.0, 1.08883]))._from_XYZ(xyz)
     return prolab
 
 
-def srgb2lab(sRGB: Tensor) -> Tensor:
-    XYZ: Tensor = color.rgb2xyz(sRGB)
-    Lab = torch.from_numpy(colour.XYZ_to_Lab(XYZ))
+def srgb2lab(srgb: Tensor) -> Tensor:
+    xyz: Tensor = sRGB()._to_XYZ(srgb)
+    Lab = CIELAB(torch.tensor([0.95047, 1.0, 1.08883]))._from_XYZ(xyz)
     return Lab
 
 
@@ -162,11 +131,7 @@ def RMS_map(
             img2_contrasts = pixel_contrasts(
                 lab2, (int(i * step), int(j * step)), tuple(filtered_neighbors)
             )
-            normalized_contrast_diffs = (
-                (img1_contrasts - img2_contrasts)
-                / 160
-                # (img1_contrasts + img2_contrasts + 1) in weighted version
-            )
+            normalized_contrast_diffs = (img1_contrasts - img2_contrasts) / 160
             rms[i, j] = torch.sqrt(torch.mean(normalized_contrast_diffs**2))
 
     return rms
@@ -250,39 +215,17 @@ def CD(
     if img2.dim() == 3:
         img2 = img2.unsqueeze(0)
 
-    assert img1.shape[1] == 3
-    assert img2.shape[1] == 3
+    # assert img1.shape[1] == 3, img1.shape
+    # assert img2.shape[1] == 3, img2.shape
 
     cd_maps = torch.empty((img1.shape[0]))
     for idx in range(img1.shape[0]):
         cd_maps[idx] = torch.mean(
             CD_map(
-                img1[idx].permute(1, 2, 0),
-                img2[idx].permute(1, 2, 0),
+                img1[idx],
+                img2[idx],
                 color_space,
                 lightness_weight,
             )
         )
     return cd_maps
-
-
-def preprocess_img(img: Tensor) -> Tensor:
-    img = img - img.min()
-    return img / img.max()
-
-
-if __name__ == "__main__":
-    from skimage.io import imread
-
-    img1_path = "/home/vkokhan/projects/dichrome/dichrome/trunk/rankendall_test/test_data/farup_simple/04.png"
-    img1 = preprocess_img(
-        torch.as_tensor(imread(img1_path))
-    )  # .permute(2, 0, 1)
-    img2_path = "/home/vkokhan/projects/dichrome/dichrome/trunk/rankendall_test/test_data/farup_simple_d/04.png"
-    img2 = preprocess_img(
-        torch.as_tensor(imread(img2_path))
-    )  # .permute(2, 0, 1)
-
-    color_space = "lab"
-
-    print(CD(img1, img2, color_space=color_space))
