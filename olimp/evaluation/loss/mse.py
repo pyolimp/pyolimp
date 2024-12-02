faf787305786b357a87725277a1c51fa9723b22e
nafe93 <nafe93@windowslive.com>
2024-12-02 14:20:12 +0300

    FIX:evaluation/loss: add new simple losses

--- olimp/evaluation/loss/mse.py
+++ olimp/evaluation/loss/mse.py
@@ -8,14 +8,10 @@ from torch import Tensor
 class MSE(Module):
     """
     Mean Squared Error (MSE) metric implemented as a PyTorch module.
-
-    Args:
-        invert (bool): If True, returns `1 - MSE` for compatibility with certain metrics.
     """
 
-    def __init__(self, invert: bool = True) -> None:
+    def __init__(self) -> None:
         super(MSE, self).__init__()  # type: ignore
-        self.invert = invert
 
     def forward(self, x: Tensor, y: Tensor) -> Tensor:
         """
@@ -29,4 +25,4 @@ class MSE(Module):
             torch.Tensor: The computed MSE value. If invert is True, returns `1 - MSE`.
         """
         mse_value = torch.mean((x - y) ** 2)
-        return 1 - mse_value if self.invert else mse_value
+        return mse_value
