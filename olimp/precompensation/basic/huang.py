550bea96a002681499f998675a0280dd79ac448e
Nafanail <nafe93@windowslive.com>
2025-01-29 14:02:28 +0300

    feat: optimization: global tone mapping methods
    * ADD:optimization:global tone mapping methods
    * ENH:optimization:refactor code

--- olimp/precompensation/basic/huang.py
+++ olimp/precompensation/basic/huang.py
@@ -7,7 +7,7 @@ from olimp.processing import scale_value
 
 def huang(image: Tensor, psf: Tensor, k: float = 0.01) -> Tensor:
     """
-    Inverse blur filtering. Single channel only.
+    Inverse blur filtering. Suports multi channel image
     """
     assert (
         image.shape[-2:] == psf.shape[-2:]
@@ -20,17 +20,16 @@ def huang(image: Tensor, psf: Tensor, k: float = 0.01) -> Tensor:
 
     # normalize MTF & OTF
     mtf_max = mtf.max()  # change to mtf[..., 0, 0] once tested properly
-    mtf = mtf / mtf_max
-    otf = otf / mtf_max
+    mtf /= mtf_max
+    otf /= mtf_max
 
     # we manually set values for ratio in IBF as {num} / {denum}
     # to be able to handle zero division cases
     num = torch.fft.fftn(image, dim=(-2, -1)) * torch.square(mtf)
-    denum = otf * (torch.square(mtf) + k)
+    denum = torch.mul(otf, torch.square(mtf) + k, out=otf)
+    denum[denum == 0.0] = torch.finfo(denum.dtype).eps
 
-    ratio = torch.zeros_like(num)
-    mask = denum != 0
-    ratio[mask] = torch.div(num[mask], denum[mask])
+    ratio = torch.div(num, denum, out=num)
 
     # the result is already shifted to center
     result = torch.fft.ifftn(ratio, dim=(-2, -1), norm="backward")
@@ -49,7 +48,7 @@ def _demo():
         progress(1.0)
         return scale_value(ret, min_val=0, max_val=1.0)
 
-    demo("Huang", demo_huang, mono=True)
+    demo("Huang", demo_huang, mono=False)
 
 
 if __name__ == "__main__":
