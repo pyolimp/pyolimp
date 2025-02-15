2790658c59d346c9517e4504dac86bc507da57ab
bunin-kirill <kirbucom@gmail.com>
2025-02-15 12:43:37 +0300

    blacke vae

--- olimp/precompensation/nn/models/vae.py
+++ olimp/precompensation/nn/models/vae.py
@@ -47,11 +47,20 @@ class VAE(nn.Module):
         w, h = self.image_size[0] / 64, self.image_size[1] / 64
         self.latent_shape = (math.ceil(w), math.ceil(h))
 
-        self.fc_mu = nn.Linear(1024 * self.latent_shape[0] * self.latent_shape[1], latent_dimension)
-        self.fc_logvar = nn.Linear(1024 * self.latent_shape[0] * self.latent_shape[1], latent_dimension)
+        self.fc_mu = nn.Linear(
+            1024 * self.latent_shape[0] * self.latent_shape[1],
+            latent_dimension,
+        )
+        self.fc_logvar = nn.Linear(
+            1024 * self.latent_shape[0] * self.latent_shape[1],
+            latent_dimension,
+        )
 
         # Decoder
-        self.decoder_input = nn.Linear(latent_dimension, 1024 * self.latent_shape[0] * self.latent_shape[1])
+        self.decoder_input = nn.Linear(
+            latent_dimension,
+            1024 * self.latent_shape[0] * self.latent_shape[1],
+        )
 
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(
