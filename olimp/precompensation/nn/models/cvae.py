b45e9caf01d832e34ee6895e7b1162f895546b3f
bunin-kirill <kirbucom@gmail.com>
2025-02-17 14:41:25 +0300

    add variable

--- olimp/precompensation/nn/models/cvae.py
+++ olimp/precompensation/nn/models/cvae.py
@@ -53,20 +53,23 @@ class CVAE(nn.Module):
         # Assuming input image size is image_size
         w, h = self.image_size[0] / 64, self.image_size[1] / 64
         self.latent_shape = (math.ceil(w), math.ceil(h))
+        latent_size_multiplication = (
+            self.latent_shape[0] * self.latent_shape[1]
+        )
 
         self.fc_mu = nn.Linear(
-            1024 * self.latent_shape[0] * self.latent_shape[1] + num_classes,
+            1024 * latent_size_multiplication + num_classes,
             latent_dimension,
         )
         self.fc_logvar = nn.Linear(
-            1024 * self.latent_shape[0] * self.latent_shape[1] + num_classes,
+            1024 * latent_size_multiplication + num_classes,
             latent_dimension,
         )
 
         # Decoder
         self.decoder_input = nn.Linear(
             latent_dimension + num_classes,
-            1024 * self.latent_shape[0] * self.latent_shape[1],
+            1024 * latent_size_multiplication,
         )
 
         self.decoder = nn.Sequential(
