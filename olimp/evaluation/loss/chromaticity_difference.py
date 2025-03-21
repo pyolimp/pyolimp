a92b35f9f7a3ca8878f5494cff21b9c7e26487f4
Sergey Gladilin <gladilin@gmail.com>
2025-03-21 16:03:22 +0300

    feat: simplify CD metric by removing `lightness_weight` parameter

--- olimp/evaluation/loss/chromaticity_difference.py
+++ olimp/evaluation/loss/chromaticity_difference.py
@@ -9,51 +9,39 @@ from ..cs.srgb import sRGB
 from ..cs.cielab import CIELAB
 from ..cs.prolab import ProLab as ProLabCS
 
+def srgb2lab_chromaticity(srgb: Tensor) -> Tensor:
+    lab = CIELAB(D65_sRGB).from_XYZ(sRGB().to_XYZ(srgb))
+    lab_chromaticity = lab[1:3, :, :]
+    return lab_chromaticity
 
-def srgb2prolab(srgb: Tensor) -> Tensor:
-    return ProLabCS(D65_sRGB).from_XYZ(sRGB().to_XYZ(srgb))
-
-
-def srgb2lab(srgb: Tensor) -> Tensor:
-    return CIELAB(D65_sRGB).from_XYZ(sRGB().to_XYZ(srgb))
-
+def srgb2prolab_chromaticity(srgb: Tensor) -> Tensor:
+    prolab = ProLabCS(D65_sRGB).from_XYZ(sRGB().to_XYZ(srgb))
+    prolab[0, :, :][prolab[0, :, :] == 0] = 1.0
+    prolab_chromaticity = prolab[1:3, :, :] / prolab[0, :, :]
+    return prolab_chromaticity
 
 def CD_map(
     img1: Tensor,
     img2: Tensor,
     color_space: Literal["lab", "prolab"],
-    lightness_weight: int = 0,
 ) -> Tensor:
-    from math import sqrt
-
     if color_space == "lab":
-        lab1 = srgb2lab(img1)
-        lab2 = srgb2lab(img2)
+        chromaticity1 = srgb2lab_chromaticity(img1)
+        chromaticity2 = srgb2lab_chromaticity(img2)
 
     elif color_space == "prolab":
-        lab1 = srgb2prolab(img1)
-        lab1[0, :, :][lab1[0, :, :] == 0] = 1.0
-        lab1[1, :, :] = lab1[1, :, :] / lab1[0, :, :]
-        lab1[2, :, :] = lab1[2, :, :] / lab1[0, :, :]
+        chromaticity1 = srgb2prolab_chromaticity(img1)
+        chromaticity2 = srgb2prolab_chromaticity(img2)
 
-        lab2 = srgb2prolab(img2)
-        lab2[0, :, :][lab2[0, :, :] == 0] = 1.0
-        lab2[1, :, :] = lab2[1, :, :] / lab2[0, :, :]
-        lab2[2, :, :] = lab2[2, :, :] / lab2[0, :, :]
-
-    diff = lab1 - lab2
-    weights = torch.tensor((sqrt(lightness_weight), 1, 1))[:, None, None]
-    weighted_diff = diff * weights
-    chromatic_diff = torch.linalg.norm(weighted_diff, dim=0)
-    return chromatic_diff
-
+    chromaticity_diff = torch.linalg.norm(chromaticity1 - chromaticity2, dim = 0)
+    
+    return chromaticity_diff
 
 class CDBase(Module):
     _color_space: Literal["lab", "prolab"]
 
-    def __init__(self, lightness_weight: int = 0) -> None:
+    def __init__(self) -> None:
         super().__init__()
-        self._lightness_weight = lightness_weight
 
     def forward(
         self,
@@ -73,7 +61,6 @@ class CDBase(Module):
                     img1[idx],
                     img2[idx],
                     self._color_space,
-                    self._lightness_weight,
                 )
             )
         return cd_maps
