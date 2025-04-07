d464c258e99ec150abd6b32a43431527b4150d1b
Arseniy Terekhin <senyai@gmail.com>
2025-04-07 16:18:18 +0300

    dev: add minimal training coverage

--- coverage.sh
+++ coverage.sh
@@ -3,5 +3,6 @@
 set -e
 python3 -m coverage run -m unittest
 PYTHONPATH=. python3 -m coverage run --append docs/gen_images.py --force
+PYTHONPATH=. python3 -m coverage run --append -m olimp.precompensation.nn.train --override '{"epochs": 1, "sample_size": 2}'
 python3 -m coverage report
 python3 -m coverage html
