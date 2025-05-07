63c1c5e139f095ffd2f1e543d74f6e4d2a2ea916
bunin-kirill <kirbucom@gmail.com>
2025-05-07 03:02:11 +0300

    start kerunc

---
+++ olimp/precompensation/nn/models/kerunc/__init__.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import typing as tp
+
+import torch
+from torch import nn, Tensor
+from typing import Any, TypeAlias
+
+from .model import KernelErrorModel
+from ..download_path import download_path, PyOlimpHF
+
+Inputs: TypeAlias = tuple[Tensor, Tensor]
+
+
+class PrecompensationKERUNC(KernelErrorModel):
+    """
+    .. image:: ../../../../_static/kerunc.svg
+       :class: full-width
+    """
+
+    def __init__(self,
+                 lmds: tp.List[float],
+                 layers: int,
+                 deep: int,):
+        super().__init__(lmds=lmds, layers=layers, deep=deep)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, inputs: Inputs) -> Tensor:
+        (
+            image,
+            psf,
+        ) = inputs
+        image = super().forward(image, psf)[0]
+        return (self.sigmoid(image),)
+
+    @classmethod
+    def from_path(cls, path: PyOlimpHF, **kwargs: Any):
+        model = cls(**kwargs)
+        path = download_path(path)
+        state_dict = torch.load(
+            path, map_location=torch.get_default_device(), weights_only=True
+        )
+        model.load_state_dict(state_dict)
+        return model
+
+    def preprocess(self, image: Tensor, psf: Tensor) -> Inputs:
+        psf = torch.fft.fftshift(psf)
+        return image, psf
+
+    def postprocess(self, tensors: tuple[Tensor]) -> tuple[Tensor]:
+        return tensors
+
+    def arguments(self, input: Tensor, psf: Tensor):
+        return {}
---
+++ olimp/precompensation/nn/models/kerunc/__main__.py
@@ -0,0 +1,26 @@
+import torch
+from torch import Tensor
+from . import PrecompensationKERUNC
+
+
+def _demo():
+    from ...._demo import demo
+    from typing import Callable
+
+    def demo_kerunc(
+        image: Tensor, psf: Tensor, progress: Callable[[float], None]
+    ) -> Tensor:
+        model = PrecompensationKERUNC.from_path(path="hf://RVI/kerunc.pt")
+
+        with torch.inference_mode():
+            inputs = model.preprocess(image, psf.to(torch.float32))
+            progress(0.1)
+            (precompensation,) = model(inputs, **model.arguments(inputs, psf))
+            progress(1.0)
+            return precompensation
+
+    demo("KERUNC", demo_kerunc, mono=True, num_output_channels=3)
+
+
+if __name__ == "__main__":
+    _demo()
