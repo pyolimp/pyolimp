3e460f18bdc35c873cce54b95024cd15af8502f0
Arseniy Terekhin <senyai@gmail.com>
2025-09-19 14:13:06 +0300

    feat: add convenient `fftshift`

--- olimp/processing.py
+++ olimp/processing.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 import torch
 import torch.nn.functional as F
+from torch import Tensor
 
 
-def resize_kernel(
-    kernel: torch.Tensor, target_size: tuple[int, int]
-) -> torch.Tensor:
+def resize_kernel(kernel: Tensor, target_size: tuple[int, int]) -> Tensor:
     """
     Rescales kernel to `target_size`
     """
@@ -16,27 +15,36 @@ def resize_kernel(
     return resized_kernel
 
 
-def fft_conv(image: torch.Tensor, kernel: torch.Tensor) -> torch.Tensor:
+def fft_conv(image: Tensor, kernel: Tensor) -> Tensor:
     """
     FFT base method for fast convolution. Input types are expected to be
     `float32` and size must be equal
     """
     assert image.dtype == torch.float32, image.dtype
     assert kernel.dtype == torch.float32, kernel.dtype
-    assert (
-        image.shape[-2:] == kernel.shape[-2:]
-    ), f"Expected equal shapes, got: image={image.shape[-2:]}, kernel={kernel.shape[-2:]}"
+    assert image.shape[-2:] == kernel.shape[-2:], (
+        "Expected equal shapes, got: "
+        f"image={image.shape[-2:]}, kernel={kernel.shape[-2:]}"
+    )
 
     return torch.real(
         torch.fft.ifft2(torch.fft.fft2(image) * torch.fft.fft2(kernel))
     )
 
 
+def fftshift(x: Tensor, dims: tuple[int, ...] = (-2, -1)) -> Tensor:
+    """
+    `torch.fft.fftshift` wrapper
+
+    By default `torch.fft.fftshift` shifts all dimensions, this
+    function is provided for convenience
+    """
+    return torch.fft.fftshift(x, dim=dims)
+
+
 def scale_value(
-    arr: torch.Tensor,
-    min_val: float = 0.0,
-    max_val: float = 1.0,
-) -> torch.Tensor:
+    arr: Tensor, min_val: float = 0.0, max_val: float = 1.0
+) -> Tensor:
     """
     Rescale values making minimum equal `min_val` and maximum equal `max_val`
     """
@@ -59,9 +67,10 @@ def scale_value(
     return out
 
 
-def quantile_clip(image: torch.Tensor, quantile: float = 0.98) -> torch.Tensor:
+def quantile_clip(image: Tensor, quantile: float = 0.98) -> Tensor:
     """
-    Normalizes each image in the batch by its specified quantile value and clips the result to [0, 1].
+    Normalizes each image in the batch by its specified quantile
+    value and clips the result to [0, 1].
     """
     assert (
         0 < quantile <= 1
